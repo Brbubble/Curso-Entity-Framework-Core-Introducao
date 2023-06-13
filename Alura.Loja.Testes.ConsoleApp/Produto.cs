1a1988914eac10fd1d24c7a51043d3bd63e1eb34
Bruna Silva <bruna.silva@confitec.com.br>
2023-06-13 16:00:36 -0300

    commit

---
+++ Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class Produto
+    {
+        public int Id { get; internal set; }
+        public string Nome { get; internal set; }
+        public string Categoria { get; internal set; }
+        public double PrecoUnitatio { get; internal set; }
+        public string Unidade { get; internal set; }
+        public IList<PromocaoProduto> Produtos { get; internal set; }
+        public IList<Compra> Compras { get; internal set; }
+
+
+
+        public override string ToString()
+        {
+            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.PrecoUnitatio}" ;
+        }
+    }
+
+}
